375f3301025cc88d97386d9ce869406f013af517
namnam-990 <my3653@gmail.com>
2026-01-21 15:37:03 +0900

    question db 추가 버전

--- five_sec/models.py
+++ five_sec/models.py
@@ -1,1 +1,18 @@
 from five_sec import db
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 질문 본문
+    subject = db.Column(db.String(200), nullable=False)
+
+    # 카테고리: energy / social / action / mood
+    category = db.Column(db.String(20), nullable=False)
+
+    # 선택지 텍스트
+    left_text = db.Column(db.String(100), nullable=False)
+    right_text = db.Column(db.String(100), nullable=False)
+
+    # 선택 시 점수 (해당 카테고리 기준)
+    left_score = db.Column(db.Integer, nullable=False)
+    right_score = db.Column(db.Integer, nullable=False)
--- five_sec/views/question_views.py
+++ five_sec/views/question_views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for
 from werkzeug.utils import redirect
+from five_sec.models import Question
 
 bp = Blueprint('question', __name__, url_prefix='/')
 
@@ -7,4 +8,8 @@ bp = Blueprint('question', __name__, url_prefix='/')
 @bp.route('/question')
 def question():
     return render_template('question.html')
+@bp.route('/question/list')
+def question_list():
+    question_list = Question.query.all()
+    return render_template('question_list.html', question_list=question_list)
 
