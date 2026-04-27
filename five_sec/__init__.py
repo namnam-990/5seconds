46dd761034db4baacdc3b94f1ab75aad5ab37867
namnam-990 <my3653@gmail.com>
2026-04-27 16:45:41 +0900

    login_views.py, User 모델 추가 (구현0)

--- five_sec/__init__.py
+++ five_sec/__init__.py
@@ -1,25 +1,36 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 import config
-from five_sec.views import login_views
-
+login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
+
+
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     app.config["SECRET_KEY"] = "dev-secret-key"
 
+    login_manager.init_app(app)
+    login_manager.login_view = 'login.login'
+
     # ORM
     db.init_app(app)
     migrate.init_app(app, db)
     from . import models
 
+    # LOGIN
+    @login_manager.user_loader
+    def load_user(user_id):
+        return models.User.query.get(int(user_id))
+
     # bp
-    from .views import main_views, question_views, static_views
+    from .views import main_views, question_views, static_views, login_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(static_views.bp)
--- five_sec/views/login_views.py
+++ five_sec/views/login_views.py
@@ -1,9 +1,26 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from werkzeug.utils import redirect
+from flask_login import login_user, logout_user, current_user
+from five_sec.models import User
+bp = Blueprint('login', __name__, url_prefix='/login')
 
-bp = Blueprint('login', __name__, url_prefix='/')
+
+@bp.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        login_user(user)
+        return redirect('/')
 
 
-@bp.route('/')
-def static():
-    return render_template('static.html')
+
+    return render_template('login.html')
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
